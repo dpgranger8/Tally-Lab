3f5221c5ca59c62fb5b9475262139f2338077b19
David Granger <dpgranger8@gmail.com>
2023-02-16 08:11:20 -0700

    Initial Commit

---
+++ Tally/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tally
+//
+//  Created by David Granger on 2/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
